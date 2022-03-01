75fc0f7aefb96b8079e7b2ff1c35ee7f676de304
Magdalena Tomaszewska <szub_m@wp.pl>
2022-03-01 10:55:50 +0100

    3. Verify Add/Remove item into cart functionality - random selection of elements added

--- src/test/AddRemoveTest.java
+++ src/test/AddRemoveTest.java
@@ -8,12 +8,16 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class AddRemoveTest {
 
     public WebDriver driver;
 
     @BeforeTest
-    public void driverInitCategoryAndItemPickUpAndAddToTheCart() {
+    public void driverSetupCategoryAndItemPickUpAndAddToTheCart() {
         WebDriverManager.chromedriver().browserVersion("97").setup();
         driver = new ChromeDriver();
         driver.get("https://www.amazon.com/");
@@ -21,8 +25,10 @@ public class AddRemoveTest {
         WebElement category = driver.findElement(By.xpath("//a[@aria-label = \"Toys & Games\"]"));
         category.click();
 
-        WebElement item = driver.findElement(By.xpath("//div[1]/div/div/span/a/div/img"));
-        item.click();
+        List<WebElement> items = new ArrayList<>(driver.findElements(By.xpath("//div[contains(@class,\"SearchResultsContainer\")]//img")));
+        int index = getIndex(items.size());
+        WebElement selectedItem = items.get(index);
+        selectedItem.click();
 
         WebElement addToCartButton = driver.findElement(By.id("add-to-cart-button"));
         addToCartButton.submit();
@@ -53,11 +59,16 @@ public class AddRemoveTest {
         WebElement deleteButton = driver.findElement(By.xpath("//input[@value =\"Delete\"]"));
         deleteButton.click();
 
-        WebElement cartIsEmptyConfirmation = new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class=\"a-row sc-cart-header\"]//h1[normalize-space(text()=\"Your Amazon Cart is empty.\")]")));
+        WebElement cartIsEmptyConfirmation = new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class=\"a-row sc-cart-header\"]//h1")));
         Assert.assertEquals(cartIsEmptyConfirmation.getText(), "Your Amazon Cart is empty.");
 
         WebElement priceOfItemInTheCart = driver.findElement(By.xpath("//span[text()=\"$0.00\"]"));
         Assert.assertEquals(priceOfItemInTheCart.getText(), "$0.00");
 
+    }
+
+    private int getIndex(int index) {
+        Random random = new Random();
+        return random.nextInt(index + 1);
     }
 }
