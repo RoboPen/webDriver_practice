53a0cb0577091aab4e65de9ba40215c7f70fc863
Magdalena Tomaszewska <szub_m@wp.pl>
2022-03-02 10:27:01 +0100

    3. Verify Add/Remove item into cart functionality - try catch block added since some items can't be bought

--- src/test/AddRemoveTest.java
+++ src/test/AddRemoveTest.java
@@ -25,13 +25,20 @@ public class AddRemoveTest {
         WebElement category = driver.findElement(By.xpath("//a[@aria-label = \"Toys & Games\"]"));
         category.click();
 
-        List<WebElement> items = new ArrayList<>(driver.findElements(By.xpath("//div[contains(@class,\"SearchResultsContainer\")]//img")));
-        int index = getIndex(items.size());
-        WebElement selectedItem = items.get(index);
-        selectedItem.click();
-
-        WebElement addToCartButton = driver.findElement(By.id("add-to-cart-button"));
-        addToCartButton.submit();
+        while (true) {
+            List<WebElement> items = new ArrayList<>(driver.findElements(By.xpath("//div[contains(@class,\"SearchResultsContainer\")]//img")));
+            int index = getIndex(items.size());
+            WebElement selectedItem = items.get(index);
+            selectedItem.click();
+            try {
+                WebElement addToCartButton = driver.findElement(By.id("add-to-cart-button"));
+                addToCartButton.submit();
+                break;
+            } catch (Exception e) {
+                driver.get("https://www.amazon.com/b?node=16225015011&pf_rd_r=QN0PGK29JQ4VWNP5GJYP&pf_rd_p=e5b0c85f-569c-4c90-a58f-0c0a260e45a0&pd_rd_r=c7cb020a-72d6-45f9-a523-6e430aafa019&pd_rd_w=BaH4A&pd_rd_wg=wMcAj&ref_=pd_gw_unk");
+                System.out.println("Item can be only pre-ordered");
+            }
+        }
     }
 
     @AfterTest
@@ -42,7 +49,7 @@ public class AddRemoveTest {
 
 
     @Test
-    public void addToCartTest() {
+    public void verifyItemWasSuccessfullyAddedToCart() {
         WebElement addedToCartConfirmation = driver.findElement(By.xpath("//div[contains(@class, \"sw-atc-message-section\")]//span[normalize-space(text()=\"Added to Cart\")]"));
         Assert.assertEquals(addedToCartConfirmation.getText(), "Added to Cart");
 
@@ -52,7 +59,7 @@ public class AddRemoveTest {
 
     @SuppressWarnings("deprecation")
     @Test
-    public void removeFromCartTest() {
+    public void verifyItemWasSuccessfullyRemovedFromCart() {
         WebElement cart = driver.findElement(By.id("nav-cart"));
         cart.click();
 
@@ -64,11 +71,10 @@ public class AddRemoveTest {
 
         WebElement priceOfItemInTheCart = driver.findElement(By.xpath("//span[text()=\"$0.00\"]"));
         Assert.assertEquals(priceOfItemInTheCart.getText(), "$0.00");
-
     }
 
     private int getIndex(int index) {
         Random random = new Random();
-        return random.nextInt(index + 1);
+        return random.nextInt(index);
     }
 }
