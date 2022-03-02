128e16fc1cebd00f4c27f6035d619980a65694d3
Malwina Siermińska <malwina.sierminska@gmail.com>
2022-03-02 21:08:22 +0100

    Task 4: Updated: filteringPrice method (cents included in price), xpath changed to be relative one.

--- src/test/java/FilteringAndSearchingTest.java
+++ src/test/java/FilteringAndSearchingTest.java
@@ -3,68 +3,67 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class FilteringAndSearchingTest {
-    @Test
-    public void SearchingFeaturedBrand() {
+
+    public WebDriver driver;
+
+    @BeforeTest
+    public void driverSetupPageOpening() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
 
         driver.get("https://www.amazon.com/b?node=16225007011&pf_rd_r=SM5D2S60J1PS1V880E7X&pf_rd_p=e5b0c85f-569c-4c90-a58f-0c0a2" +
                 "60e45a0&pd_rd_r=34a43d50-1a1b-42dc-a688-e6ed63e03ca3&pd_rd_w=NoO9r&pd_rd_wg=zILV6&ref_=pd_gw_unk");
+    }
 
-        WebElement searchButton = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[2]/div[2]/div/div/div[5]/ul/li[1]/span/a/span"));
-        searchButton.click();
-
-        driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[1]/div[1]/div/span[3]"))
-                .findElements(By.className("a-size-base-plus"))
-                .forEach(webElement -> Assert.assertTrue(webElement.getText().contains("TAKAGI")));
-
+    @AfterTest
+    public void driverClose() {
         driver.close();
         driver.quit();
     }
 
     @Test
-    public void FilteringPrice() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+    public void searchingFeaturedBrand() {
 
-        driver.get("https://www.amazon.com/b?node=16225007011&pf_rd_r=SM5D2S60J1PS1V880E7X&pf_rd_p=e5b0c85f-569c-4c90-a58f-0c0a2" +
-                "60e45a0&pd_rd_r=34a43d50-1a1b-42dc-a688-e6ed63e03ca3&pd_rd_w=NoO9r&pd_rd_wg=zILV6&ref_=pd_gw_unk");
+        WebElement searchButton = driver.findElement(By.xpath("//span[text() = \"TAKAGI\"]"));
+        searchButton.click();
 
-        WebElement searchButton = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[2]/div[2]/div/div/div[12]/ul/li[1]/span"));
+
+        driver.findElements(By.xpath("//span[@data-component-type=\"s-search-results\"]"))
+                .forEach(webElement -> Assert.assertTrue(webElement.getText().contains("TAKAGI")));
+
+    }
+
+    @Test
+    public void filteringPrice() {
+
+        WebElement searchButton = driver.findElement(By.xpath("//span[text() = \"Under $25\"]"));
         searchButton.click();
 
         float minPrice = 0;
         float maxPrice = 25;
 
-        driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[1]/div[1]/div/span[3]/div[2]"))
-                .findElements(By.className("a-price-whole"))
+        driver
+                .findElements(By.xpath("//span[@class=\"a-price\"]/span[@class=\"a-offscreen\"]"))
                 .forEach(webElement -> {
-                    float price = Float.parseFloat(webElement.getText());
+                    float price = Float.parseFloat(webElement.getAttribute("textContent").replace("$", ""));
                     Assert.assertTrue(price <= maxPrice);
                     Assert.assertTrue(price >= minPrice);
 
                 });
-
-
-        driver.close();
-        driver.quit();
     }
 
     @Test
-    public void SortingByPrice() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+    public void sortingByPrice() {
 
-        driver.get("https://www.amazon.com/b?node=16225007011&pf_rd_r=SM5D2S60J1PS1V880E7X&pf_rd_p=e5b0c85f-569c-4c90-a58f-0c0a2" +
-                "60e45a0&pd_rd_r=34a43d50-1a1b-42dc-a688-e6ed63e03ca3&pd_rd_w=NoO9r&pd_rd_wg=zILV6&ref_=pd_gw_unk");
-
-        WebElement searchButton = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[2]/div[2]/div/div/div[5]/ul/li[1]/span/a/span"));
+        WebElement searchButton = driver.findElement(By.xpath("//span[text() = \"TAKAGI\"]"));
         searchButton.click();
 
         WebElement searchDropdownList = driver.findElement(By.id("a-autoid-0-announce"));
@@ -75,8 +74,7 @@ public class FilteringAndSearchingTest {
 
 
         List<Float> pricesLowestToHighest = new ArrayList<>();
-        driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[1]/div[1]/div/span[3]/div[2]"))
-                .findElements(By.className("a-price-whole"))
+        driver.findElements(By.className("a-price-whole"))
                 .forEach(webElement -> {
                     float price = Float.parseFloat(webElement.getText());
                     pricesLowestToHighest.add(price);
@@ -84,13 +82,11 @@ public class FilteringAndSearchingTest {
 
         assertAscendingPrices(pricesLowestToHighest);
 
-        driver.close();
-        driver.quit();
     }
 
     private void assertAscendingPrices(List<Float> prices) {
         for (int i = 0; i < prices.size() - 1; i++) {
-            Assert.assertTrue(prices.get(i) < prices.get(i + 1));
+            Assert.assertTrue(prices.get(i) <= prices.get(i + 1));
         }
     }
 }
