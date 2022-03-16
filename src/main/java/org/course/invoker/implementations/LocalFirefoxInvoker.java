8e1d5ec27c19ae0ef35c1623729ab66946de2f20
MagdaTom <66006478+MagdaTom@users.noreply.github.com>
2022-03-16 11:36:56 +0100

    Create LocalFirefoxInvoker.java

---
+++ src/main/java/org/course/invoker/implementations/LocalFirefoxInvoker.java
@@ -0,0 +1,14 @@
+package org.course.invoker.implementations;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.course.invoker.WebDriverInvoker;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class LocalFirefoxInvoker implements WebDriverInvoker {
+    @Override
+    public WebDriver invokeWebDriver() {
+        WebDriverManager.firefoxdriver().setup();
+        return new FirefoxDriver();
+    }
+}
