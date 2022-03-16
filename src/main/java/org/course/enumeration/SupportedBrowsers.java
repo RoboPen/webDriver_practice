979440e8ef013cb89cec63f2133636a05d3e8152
mm <henrykus888@gmail.com>
2022-03-16 10:17:02 +0100

    Adding SupprtedBrowsers file

---
+++ src/main/java/org/course/enumeration/SupportedBrowsers.java
@@ -0,0 +1,21 @@
+package org.course.enumeration;
+
+import org.course.invoker.WebDriverInvoker;
+import org.course.invoker.implementations.LocalChromeInvoker;
+import org.course.invoker.implementations.LocalFirefoxInvoker;
+import org.openqa.selenium.WebDriver;
+
+public enum SupportedBrowsers {
+    LOCAL_FIREFOX(new LocalFirefoxInvoker()),
+    LOCAL_CHROME(new LocalChromeInvoker());
+
+    private final WebDriverInvoker webDriverInvoker;
+
+    SupportedBrowsers(WebDriverInvoker webDriverInvoker) {
+        this.webDriverInvoker = webDriverInvoker;
+    }
+
+    public WebDriver getWebDriver() {
+        return webDriverInvoker.invokeWebDriver();
+    }
+}
