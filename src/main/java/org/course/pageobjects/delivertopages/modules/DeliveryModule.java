9672e3294ce4f8aa003d1d0f7202507bea436584
Arek <96ar.gaw@gmail.com>
2022-03-18 10:54:02 +0100

    Created page models and tests for deliver to functionality

---
+++ src/main/java/org/course/pageobjects/delivertopages/modules/DeliveryModule.java
@@ -0,0 +1,60 @@
+package org.course.pageobjects.delivertopages.modules;
+
+import org.course.pageobjects.delivertopages.BasePage;
+import org.course.pageobjects.delivertopages.MainPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+public class DeliveryModule extends BasePage {
+    WebElement zipCodeInput;
+
+    WebElement applyButton;
+
+    WebElement countryDropdownButton;
+
+    WebElement doneButton;
+
+    public DeliveryModule(WebDriver driver) {
+        super(driver);
+        zipCodeInput = new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.elementToBeClickable(By.id("GLUXZipUpdateInput")));
+        applyButton = new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.elementToBeClickable(By.id("GLUXZipUpdate")));
+        countryDropdownButton = new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.elementToBeClickable(By.id("GLUXCountryListDropdown")));
+        zipCodeInput = new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.elementToBeClickable(By.id("GLUXZipUpdateInput")));
+        doneButton = new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.elementToBeClickable(By.name("glowDoneButton")));
+    }
+
+    public List<WebElement> getCountiresList() {
+        countryDropdownButton.click();
+        return driver.findElements(By.xpath("//ul[contains(@class, \"a-list-link\")]/li"));
+    }
+
+    public MainPage setZipCode(String zipCode) {
+        zipCodeInput.sendKeys(zipCode);
+        applyButton.click();
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.elementToBeClickable(By.xpath("(//input[@id=\"GLUXConfirmClose\"]/../..)[2]"))).click();
+        return new MainPage(driver);
+    }
+
+    public MainPage setCountry(String country) {
+        countryDropdownButton.click();
+        WebElement countryLink = driver.findElement(By.xpath(String.format("//a[text()=\"%s\"]", country)));
+        countryLink.click();
+        WebElement doneButton = driver.findElement(By.xpath("//button[@name=\"glowDoneButton\"]"));
+        doneButton.click();
+        return new MainPage(driver);
+    }
+}
+
